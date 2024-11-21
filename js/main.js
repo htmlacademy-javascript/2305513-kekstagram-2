1c97f3249b5bafe42dab8985d6361cc135e0ce5a
Дмитрий Шамаев <dimashamaeff@gmail.com>
2024-11-21 17:55:13 +0300

    Добавляет домашнее задание

--- js/main.js
+++ js/main.js
@@ -26,14 +26,14 @@ const DESCRIPTION = [
 
 //рандомное число
 
-function getRandomInteger(min, max) {
+const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-}
+};
 
-function createRandomIdFromRangeGenerator(min, max) {
+const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
 
   return function () {
@@ -48,13 +48,7 @@ function createRandomIdFromRangeGenerator(min, max) {
     previousValues.push(currentValue);
     return currentValue;
   };
-}
-
-// function createIdGenerator(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
+};
 
 const createIdGenerator = (min, max) => {
   min = Math.ceil(min);
