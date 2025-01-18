05fb09f2f5c78f22b8ca896479e58da21960b2c4
Дмитрий Шамаев <dimashamaeff@gmail.com>
2025-01-18 18:20:36 +0300

    Добавляет упрощение и разделение кода функции

--- js/bigPosts.js
+++ js/bigPosts.js
@@ -3,66 +3,75 @@ const commentsContainer = bigPicture.querySelector('.social__comments');
 const closeBigPicture = bigPicture.querySelector('.big-picture__cancel');
 const container = document.querySelector('.pictures');
 
-const bigPictureHandler = (generatedPosts) => {
-  const openBigPicture = (pictureId) => {
-    const currentPicture = generatedPosts.find((photo) => photo.id === Number(pictureId));
+//отрисовка комментариев
+const renderComments = (comments) => {
+  commentsContainer.innerHTML = ''; // Очищаем предыдущие комментарии
+  const fragment = document.createDocumentFragment();
 
-    bigPicture.classList.remove('hidden');
+  comments.forEach((comment) => {
+    const commentElement = document.createElement('li');
+    commentElement.classList.add('social__comment');
 
-    bigPicture.querySelector('.big-picture__img img').src = currentPicture.url;
-    bigPicture.querySelector('.likes-count').textContent = currentPicture.likes;
-    bigPicture.querySelector('.social__comment-shown-count').textContent = currentPicture.comments.length;
-    bigPicture.querySelector('.social__comment-total-count').textContent = currentPicture.comments.length;
+    const pictureNode = document.createElement('img');
+    pictureNode.classList.add('social__picture');
+    pictureNode.src = comment.avatar;
+    pictureNode.alt = comment.name;
 
-    commentsContainer.innerHTML = ''; // Очищает предыдущие комментарии
-    const fragment = document.createDocumentFragment();
+    const textNode = document.createElement('p');
+    textNode.classList.add('social__text');
+    textNode.textContent = comment.message;
 
-    currentPicture.comments.forEach((comment) => {
-      const commentElement = document.createElement('li');
-      commentElement.classList.add('social__comment');
+    commentElement.appendChild(pictureNode);
+    commentElement.appendChild(textNode);
+    fragment.appendChild(commentElement);
+  });
 
-      const pictureNode = document.createElement('img');
-      pictureNode.classList.add('social__picture');
-      pictureNode.src = comment.avatar;
-      pictureNode.alt = comment.name;
+  commentsContainer.appendChild(fragment);
+};
 
-      const textNode = document.createElement('p');
-      textNode.classList.add('social__text');
-      textNode.textContent = comment.message;
+const openBigPicture = (currentPicture) => {
+  bigPicture.classList.remove('hidden');
+  bigPicture.querySelector('.big-picture__img img').src = currentPicture.url;
+  bigPicture.querySelector('.likes-count').textContent = currentPicture.likes;
+  bigPicture.querySelector('.social__comment-shown-count').textContent = currentPicture.comments.length;
+  bigPicture.querySelector('.social__comment-total-count').textContent = currentPicture.comments.length;
 
-      commentElement.appendChild(pictureNode);
-      commentElement.appendChild(textNode);
-      fragment.appendChild(commentElement);
-    });
+  renderComments(currentPicture.comments);
+  bigPicture.querySelector('.social__caption').textContent = currentPicture.description;
 
-    commentsContainer.appendChild(fragment);
-    bigPicture.querySelector('.social__caption').textContent = currentPicture.description;
+  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
+  bigPicture.querySelector('.comments-loader').classList.add('hidden');
+  document.body.classList.add('modal-open');
+};
 
-    bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-    bigPicture.querySelector('.comments-loader').classList.add('hidden');
-    document.body.classList.add('modal-open');
+const closeBigPictureHandler = () => {
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+};
 
-  };
+const closeBigPictureOnEsc = (event) => {
+  if (event.key === 'Escape') {
+    closeBigPictureHandler();
+  }
+};
 
-  const closeBigPictureHandler = () => {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  };
-
-  const closeBigPictureOnEsc = (event) => {
-    if (event.key === 'Escape') {
-      closeBigPictureHandler();
-    }
-  };
-
+const initBigPictureEvents = () => {
   closeBigPicture.addEventListener('click', closeBigPictureHandler);
   document.addEventListener('keydown', closeBigPictureOnEsc);
+};
+
+const bigPictureHandler = (generatedPosts) => {
+  initBigPictureEvents();
 
   container.addEventListener('click', (evt) => {
     const currentPictures = evt.target.closest('.picture');
 
     if (currentPictures) {
-      openBigPicture(currentPictures.dataset.pictureId);
+      const pictureId = Number(currentPictures.dataset.pictureId);
+      const currentPicture = generatedPosts.find((photo) => photo.id === pictureId);
+      if (currentPicture) {
+        openBigPicture(currentPicture);
+      }
     }
   });
 };
