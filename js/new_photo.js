0c48d07043177aa3ef8fc34637ed774278f9fbb0
Дмитрий Шамаев <dimashamaeff@gmail.com>
2025-03-03 16:03:59 +0300

    Добавляет 2 часть домашнего задания

---
+++ js/new_photo.js
@@ -0,0 +1,23 @@
+const inputNewPhoto = document.querySelector('.img-upload__input');
+const labelNewPhoto = document.querySelector('.img-upload__preview > img');
+const uploadPreviewPhoto = document.querySelectorAll('.effects__preview');
+
+const FILE_TYPES = ['.jpg', '.jpeg', '.png'];
+
+const fileInputChange = () => {
+  const file = inputNewPhoto.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((item) => fileName.endsWith(item));
+
+  if (matches) {
+    const url = URL.createObjectURL(file);
+    labelNewPhoto.src = url;
+    uploadPreviewPhoto.forEach((item) => {
+      item.style.backgroundImage = `url(${url})`;
+    });
+  } else {
+    document.querySelector('.data-error__title')
+  }
+};
+
+export { fileInputChange };
